a93faade82932ee5c003d8d873193b0a1041daec
LaRoncewhite <terrellwhite.aob.ceo@gmail.com>
2024-06-10 14:05:27 -0500

    Exercise 1-10

---
+++ app.js
@@ -0,0 +1,53 @@
+/*
+Exercise 1: Define an empty array
+
+1) Create an empty array and assign it to a variable called `foods`.
+
+Exercise 1 has been completed for you:
+*/
+
+/*
+const foods = [];  
+
+//console.log('Exercise 1 result:', foods);
+
+
+foods[0] = 'pizza'
+foods[1] = 'cheeseburger'
+
+//console.log('Exercise 2 result:', foods)
+
+foods.unshift('taco')
+
+//console.log('Exercise 3 results:', foods)
+
+let favFood = foods[1]
+
+//console.log('Exercise 4 result:', favFood)
+
+foods.splice(2,0, 'tofu')
+
+//console.log('Exercise 5 result:', foods)
+
+
+foods[1] = 'sushi' 
+foods.splice(2,0, 'cupcake')
+
+//console.log('Exercise 6 result:', foods)
+
+let yummy = foods.slice(1,3)
+
+//console.log('Exercise 7 result:', yummy)
+
+let soyIdx = foods.indexOf('tofu')
+
+console.log('Exercise 8 results:', soyIdx)
+
+console.log('Exercise 9 results:', foods.join('->'))
+
+let hasSoup = foods.includes('soup')
+
+console.log('Exercise 10 results:', hasSoup)
+*/
+
+
